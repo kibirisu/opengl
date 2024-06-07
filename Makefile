e4e9d22c7eb3525357d12b3b89f662b3dcb9f910
kibirisu <szgie@proton.me>
2024-06-07 22:29:07 +0200

    duh

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 LIBS=-lGL -lglfw -lGLEW
-HEADERS=allmodels.h constants.h cube.h lodepng.h model.h myCube.h shaderprogram.h sphere.h teapot.h torus.h camera.h
-FILES=cube.cpp lodepng.cpp main_file.cpp model.cpp shaderprogram.cpp sphere.cpp teapot.cpp torus.cpp camera.cpp
+HEADERS=allmodels.h constants.h cube.h lodepng.h model.h myCube.h shaderprogram.h sphere.h teapot.h torus.h
+FILES=cube.cpp lodepng.cpp main_file.cpp model.cpp shaderprogram.cpp sphere.cpp teapot.cpp torus.cpp
 main_file: $(FILES) $(HEADERS)
 	g++ -o main_file $(FILES)  $(LIBS) -I.
--- main_file.cpp
+++ main_file.cpp
@@ -27,39 +27,113 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #include "allmodels.h"
 #include "lodepng.h"
 #include "shaderprogram.h"
-/*#include "camera.h"*/
 
+glm::vec3 cameraPos = glm::vec3(0.0f, 0.0f, 3.0f);
+glm::vec3 cameraFront = glm::vec3(0.0f, 0.0f, -1.0f);
+glm::vec3 cameraUp = glm::vec3(0.0f, 1.0f, 0.0f);
+
+bool firstMouse = true;
+float yaw   = -90.0f;	// yaw is initialized to -90.0 degrees since a yaw of 0.0 results in a direction vector pointing to the right so we initially rotate a bit to the left.
+float pitch =  0.0f;
+float lastX =  800.0f / 2.0;
+float lastY =  600.0 / 2.0;
+float fov   =  45.0f;
+
+float deltaTime = 0.0f;	// time between current frame and last frame
+float lastFrame = 0.0f;
 
 float speed = 0;//[radians/s]
-float turn = 0; //skret kó¿
+float turn = 0; //skret kół
 
-Models::Torus carWheel(0.3, 0.1, 12, 12);
+void processInput(GLFWwindow *window)
+{
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, true);
+
+    float cameraSpeed = static_cast<float>(2.5 * deltaTime);
+    if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+        cameraPos += cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+        cameraPos -= cameraSpeed * cameraFront;
+    if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+        cameraPos -= glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+    if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+        cameraPos += glm::normalize(glm::cross(cameraFront, cameraUp)) * cameraSpeed;
+}
+
+// glfw: whenever the mouse moves, this callback is called
+// -------------------------------------------------------
+void mouse_callback(GLFWwindow* window, double xposIn, double yposIn)
+{
+    float xpos = static_cast<float>(xposIn);
+    float ypos = static_cast<float>(yposIn);
+
+    if (firstMouse)
+    {
+        lastX = xpos;
+        lastY = ypos;
+        firstMouse = false;
+    }
+
+    float xoffset = xpos - lastX;
+    float yoffset = lastY - ypos; // reversed since y-coordinates go from bottom to top
+    lastX = xpos;
+    lastY = ypos;
+
+    float sensitivity = 0.1f; // change this value to your liking
+    xoffset *= sensitivity;
+    yoffset *= sensitivity;
+
+    yaw += xoffset;
+    pitch += yoffset;
+
+    // make sure that when pitch is out of bounds, screen doesn't get flipped
+    if (pitch > 89.0f)
+        pitch = 89.0f;
+    if (pitch < -89.0f)
+        pitch = -89.0f;
+
+    glm::vec3 front;
+    front.x = cos(glm::radians(yaw)) * cos(glm::radians(pitch));
+    front.y = sin(glm::radians(pitch));
+    front.z = sin(glm::radians(yaw)) * cos(glm::radians(pitch));
+    cameraFront = glm::normalize(front);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+  fov -= (float)yoffset;
+  if (fov < 1.0f)
+    fov = 1.0f;
+  if (fov > 45.0f)
+    fov = 45.0f;
+}
 
 //Error processing callback procedure
 void error_callback(int error, const char* description) {
 	fputs(description, stderr);
 }
 
-//Procedura obs¿ugi klawiatury
-void key_callback(GLFWwindow* window, int key,
-	int scancode, int action, int mods) {
-
-	if (action == GLFW_PRESS) {
-		if (key == GLFW_KEY_LEFT) speed = -PI; //Je¿eli wci¿ni¿to klawisz "w lewo" ustaw pr¿dko¿¿ na -PI
-		if (key == GLFW_KEY_RIGHT) speed = PI; //Je¿eli wci¿ni¿to klawisz "w prawo" ustaw pr¿dko¿¿ na PI
-		if (key == GLFW_KEY_A) {
-			turn = PI / 6;
-		}
-		if (key == GLFW_KEY_D) {
-			turn = -PI / 6;
-		}
-	}
-
-	if (action == GLFW_RELEASE) {
-		if (key == GLFW_KEY_LEFT || key == GLFW_KEY_RIGHT) speed = 0;
-		if (key == GLFW_KEY_A || key == GLFW_KEY_D) turn = 0;
-	}
-}
+//Procedura obsługi klawiatury
+/*void key_callback(GLFWwindow* window, int key,*/
+/*	int scancode, int action, int mods) {*/
+/**/
+/*	if (action == GLFW_PRESS) {*/
+/*		if (key == GLFW_KEY_LEFT) c.Position += 1; //Jeżeli wciśnięto klawisz "w lewo" ustaw prędkość na -PI*/
+/*		if (key == GLFW_KEY_RIGHT) c.Position -= 1; //Jeżeli wciśnięto klawisz "w prawo" ustaw prędkość na PI*/
+/*		if (key == GLFW_KEY_A) {*/
+/*			turn = PI / 6;*/
+/*		}*/
+/*		if (key == GLFW_KEY_D) {*/
+/*			turn = -PI / 6;*/
+/*		}*/
+/*	}*/
+/**/
+/*	if (action == GLFW_RELEASE) {*/
+/*		if (key == GLFW_KEY_LEFT || key == GLFW_KEY_RIGHT) speed = 0;*/
+/*		if (key == GLFW_KEY_A || key == GLFW_KEY_D) turn = 0;*/
+/*	}*/
+/*}*/
 
 
 
@@ -70,7 +144,7 @@ void initOpenGLProgram(GLFWwindow* window) {
 	//************Place any code here that needs to be executed once, at the program start************
 	glClearColor(0, 0, 0, 1); //Set color buffer clear color
 	glEnable(GL_DEPTH_TEST); //Turn on pixel depth test based on depth buffer
-	glfwSetKeyCallback(window, key_callback);
+	/*glfwSetKeyCallback(window, key_callback);*/
 }
 
 //Release resources allocated by the program
@@ -101,8 +175,6 @@ void car2(float angle, float wheelAngle) {
 	glUniformMatrix4fv(spLambert->u("M"), 1, false, glm::value_ptr(Mk1));
 	Models::cube.drawSolid();
 
-	//
-
 	glm::mat4 Mp2 = Ms;
 	Mp2 = glm::translate(Mp2, glm::vec3(1 * 13.0f, 0.0f, 0.0f));
 	Mp2 = glm::scale(Mp2, glm::vec3(5.0f, 1.0f, 5.0f));
@@ -117,8 +189,6 @@ void car2(float angle, float wheelAngle) {
 	glUniform4f(spLambert->u("color"), 1, 0, 1, 1);
 	glUniformMatrix4fv(spLambert->u("M"), 1, false, glm::value_ptr(Mk2));
 	Models::cube.drawSolid();
-
-	//
 
 	glm::mat4 Mp3 = Ms;
 	Mp3 = glm::translate(Mp3, glm::vec3(2 * 13.0f, 0.0f, 0.0f));
@@ -135,8 +205,6 @@ void car2(float angle, float wheelAngle) {
 	glUniformMatrix4fv(spLambert->u("M"), 1, false, glm::value_ptr(Mk3));
 	Models::cube.drawSolid();
 
-	//
-
 	glm::mat4 Mp4 = Ms;
 	Mp4 = glm::translate(Mp4, glm::vec3(3 * 13.0f, 0.0f, 0.0f));
 	Mp4 = glm::scale(Mp4, glm::vec3(5.0f, 1.0f, 5.0f));
@@ -147,6 +215,21 @@ void car2(float angle, float wheelAngle) {
 
 }
 
+void room() {
+	glm::mat4 Ms = glm::mat4(1.0f);
+
+	glm::mat4 Mf1 = glm::scale(Ms, glm::vec3(5.0f, 0.025f, 5.0f));
+	/*Mp = glm::translate(Mp, glm::vec3(0.0f, -0.0f, 0.0f));*/
+	glUniform4f(spLambert->u("color"), 1, 1, 1, 1);
+	glUniformMatrix4fv(spLambert->u("M"), 1, false, glm::value_ptr(Mf1));
+	Models::cube.drawSolid();
+
+	glm::mat4 Mf2 = glm::scale(Ms, glm::vec3(5.0f, 0.025f, 5.0f));
+	Mf2 = glm::translate(Mf2, glm::vec3(0.0f, 12.0f, 0.0f));
+	glUniform4f(spLambert->u("color"), 1, 1, 1, 1);
+	glUniformMatrix4fv(spLambert->u("M"), 1, false, glm::value_ptr(Mf2));
+	Models::cube.drawSolid();
+}
 
 
 //Drawing procedure
@@ -154,17 +237,16 @@ void drawScene(GLFWwindow* window, float angle, float wheelAngle) {
 	//************Place any code here that draws something inside the window******************l
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); //Clear color and depth buffers
 
-	glm::mat4 P = glm::perspective(glm::radians(50.0f), 1.0f, 1.0f, 50.0f); //Compute projection matrix
-	glm::mat4 V = glm::lookAt(glm::vec3(0.0f, 0.0f, -7.0f), glm::vec3(0.0f, 0.0f, 0.0f), glm::vec3(0.0f, 1.0f, 0.0f)); //Compute view matrix
-  /*camera.Inputs(window);*/
-  /*camera.Matrix(45.0f, 0.1f, 100.0f);*/
+  glm::mat4 P = glm::perspective(glm::radians(fov), 1.0f, 0.1f, 100.0f);
+  glm::mat4 V = glm::lookAt(cameraPos, cameraPos + cameraFront, cameraUp);
 
-	spLambert->use();//Aktywacja programu cieniuj¿cego
+	spLambert->use();//Aktywacja programu cieniującego
 	glUniformMatrix4fv(spLambert->u("P"), 1, false, glm::value_ptr(P));
 	glUniformMatrix4fv(spLambert->u("V"), 1, false, glm::value_ptr(V));
 
 
-	car2(angle, wheelAngle);
+	/*car2(angle, wheelAngle);*/
+  room();
 
 
 	glfwSwapBuffers(window); //Copy back buffer to the front buffer
@@ -172,7 +254,6 @@ void drawScene(GLFWwindow* window, float angle, float wheelAngle) {
 
 int main(void)
 {
-  /*Camera camera(800, 800, glm::vec3(0.0f, 0.0f, 2.0f));*/
 	GLFWwindow* window; //Pointer to object that represents the application window
 
 	glfwSetErrorCallback(error_callback);//Register error processing callback procedure
@@ -182,7 +263,7 @@ int main(void)
 		exit(EXIT_FAILURE);
 	}
 
-	window = glfwCreateWindow(500, 500, "OpenGL", NULL, NULL);  //Create a window 500pxx500px titled "OpenGL" and an OpenGL context associated with it. 
+	window = glfwCreateWindow(1000, 1000, "OpenGL", NULL, NULL);  //Create a window 500pxx500px titled "OpenGL" and an OpenGL context associated with it. 
 
 	if (!window) //If no window is opened then close the program
 	{
@@ -201,15 +282,23 @@ int main(void)
 
 	initOpenGLProgram(window); //Call initialization procedure
 
+  glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  glfwSetScrollCallback(window, scroll_callback);
+  glfwSetCursorPosCallback(window, mouse_callback);
+
 	//Main application loop
 	float angle = 0; //declare variable for storing current rotation angle
 	float wheelAngle = 0;
 	glfwSetTime(0); //clear internal timer
 	while (!glfwWindowShouldClose(window)) //As long as the window shouldnt be closed yet...
 	{
+    float currentFrame = static_cast<float>(glfwGetTime());
+    deltaTime = currentFrame - lastFrame;
+    lastFrame = currentFrame;
+    processInput(window);
 		angle += speed * glfwGetTime(); //Compute an angle by which the object was rotated during the previous frame
 		wheelAngle += PI / 6 * glfwGetTime(); //Compute an angle by which the object was rotated during the previous frame
-		glfwSetTime(0); //clear internal timer
+		/*glfwSetTime(0); //clear internal timer*/
 		drawScene(window, angle, wheelAngle); //Execute drawing procedure
 		glfwPollEvents(); //Process callback procedures corresponding to the events that took place up to now
 	}
